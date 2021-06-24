701b5a3fb66022e3caba2043a450a226ea4257b3
Philip Rothschild <philipr@egde.no>
2021-06-24 13:13:01 +0200

    Altering .gitignore and renaming parameter

--- TransformBlobToSQL/TransformSsbBlobToSQL/SsbBlobToSQL.cs
+++ TransformBlobToSQL/TransformSsbBlobToSQL/SsbBlobToSQL.cs
@@ -8,11 +8,11 @@ namespace TransformSsbBlobToSQL
     public static class SsbBlobToSQL
     {
         [Function("SsbBlobToSQL")]
-        public static void Run([BlobTrigger("ssbdata/{name}", Connection = "")] string myBlob, string name,
+        public static void Run([BlobTrigger("ssbdata/{name}", Connection = "")] string ssbBlob, string name,
             FunctionContext context)
         {
             var logger = context.GetLogger("Function1");
-            logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data: {myBlob}");
+            logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data: {ssbBlob}");
         }
     }
 }
